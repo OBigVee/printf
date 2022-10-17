7209ecad08c5b38c40c38f52aea0ec17bf961fe1
OBigVee <adedejivictor98@gmail.com>
2022-10-17 23:17:19 +0100

    header file update :heavy_plus_sign:

--- main.h
+++ main.h
@@ -2,6 +2,30 @@
 #define PRINTF_H
 
 #include <stdarg.h>
+#include <unistd.h>
+
+int _putchar(char c);
+
+int print_char(va_list ap);
+
+int print_string(va_list ap);
+
+int print_integer(va_list ap);
 
 int _printf(const char *format, ...);
+
+/**
+ * struct print_format - print properties
+ * @format_id: format identifier
+ * @printer: function pointer that takes the function as parameter
+ * the function prints the correct format base on format_id.
+ */
+struct print_format
+{
+	char *format_id;
+	int (*printer)(va_list);
+}
+
+typedef struct print_format print_format;
+
 #endif /* PRINTF_H */
